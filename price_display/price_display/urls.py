3c39a0edb8af68a0f87f3e9aeced4ea494eb53fb
ravenstudios <ravenstudios@ravenstudioss-MacBook-Pro.local>
2024-02-20 13:59:19 -0600

    changed url path to root

--- price_display/price_display/urls.py
+++ price_display/price_display/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 app_name = 'display_app'
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('display_app/', include('display_app.urls')),
+    path('', include('display_app.urls')),
     # path('finances/', include('finances.urls')),
     # path('', views.index, name='index'),
     # path('display', views.display, name='display'),
